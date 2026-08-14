38a78b5d5d79ba0a3c097c6cfc45f9b0b3b8eb6f
Haylee-Govender-007 <ST10480667@myemeris.edu.za>
2026-08-14 08:32:57 +0000

    Second Commit
    Add rows and columns agents

--- src/main/java/com/mycompany/ice_task2/Ice_Task2.java
+++ src/main/java/com/mycompany/ice_task2/Ice_Task2.java
@@ -12,5 +12,22 @@ public class Ice_Task2 {
 
     public static void main(String[] args) {
         
+       
+        // Rows = agents
+        double [][] sales ={ 
+            { 800000.0, 1500000.0, 2000000.0} ,
+            {700000.0, 1200000.0, 1600000.0}
+        };
+        
+        // Estate agents
+        String [] agents = {"Joe Bloggs" , "Jane Doe"} ;
+        
+        // Columns = January, February, March
+        String[] months = {"JAN", "FEB", "MAR"};
+
+       
     }
 }
+        
+    
+
