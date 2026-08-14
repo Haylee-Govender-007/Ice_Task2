3e6cfc65abc3da3ece7cfbcae7d5874017764ad4
Haylee-Govender-007 <ST10480667@myemeris.edu.za>
2026-08-14 08:54:19 +0000

    Folders added

---
+++ Question1/src/main/java/com/mycompany/ice_task2_inheritance/Ice_Task2_Inheritance.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice_task2_inheritance;
+
+/**
+ *
+ * @author emeris
+ */
+public class Ice_Task2_Inheritance {
+
+    public static void main(String[] args) {
+        
+    }
+}
