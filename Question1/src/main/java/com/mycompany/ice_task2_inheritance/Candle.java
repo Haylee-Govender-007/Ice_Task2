a3ba7eee7de391bfee487e828e84c9f85c6fe9b5
Haylee-Govender-007 <ST10480667@myemeris.edu.za>
2026-08-14 09:03:28 +0000

    Q2 First commit
    Candle Class completed

--- Question1/src/main/java/com/mycompany/ice_task2_inheritance/Candle.java
+++ Question1/src/main/java/com/mycompany/ice_task2_inheritance/Candle.java
@@ -10,4 +10,33 @@ package com.mycompany.ice_task2_inheritance;
  */
 public class Candle {
     
+    // Data fields
+    private String colour;
+    private double height;
+    private double price;
+
+    // Getters and Setters
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+        this.price = height * 2.00; 
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    
+    
 }
