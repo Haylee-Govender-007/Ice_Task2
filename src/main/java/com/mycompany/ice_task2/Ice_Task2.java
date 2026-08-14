b72fef1eee75ef0a462936849c87c2a6a329605b
Haylee-Govender-007 <ST10480667@myemeris.edu.za>
2026-08-14 08:15:59 +0000

    First_Commit

---
+++ src/main/java/com/mycompany/ice_task2/Ice_Task2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice_task2;
+
+/**
+ *
+ * @author emeris
+ */
+public class Ice_Task2 {
+
+    public static void main(String[] args) {
+        
+    }
+}
